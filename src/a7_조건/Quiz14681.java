63834c99b365020d05fe0f03afe322856e1498ed
y2kwolf <wolfjin@naver.com>
2021-08-24 00:27:33 +0900

    21-08-24 01

--- src/a7_조건/Quiz14681.java
+++ src/a7_조건/Quiz14681.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class Quiz14681 {
 
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		Scanner axixValue = new Scanner(System.axixValue);
 		System.out.print("x축: ");
-		int x = in.nextInt();
+		int x = axixValue.nextInt();
 		
 		System.out.print("y축: ");
-		int y = in.nextInt();
+		int y = axixValue.nextInt();
 		
 		if(x > 0 && y > 0) {
 			System.out.println("1사분면");
