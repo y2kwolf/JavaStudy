703e71d8bc407fa16a85673275226297a6f3ceda
= <=>
2021-08-23 20:00:50 +0900

    21-08-23 02

---
+++ src/a8_반복/IfLoop.java
@@ -0,0 +1,16 @@
+package a8_นÝบน;
+
+public class IfLoop {
+
+	public static void main(String[] args) {
+
+			int num = 1;
+			int result = 0;
+			while(num <= 10) {
+				result += num;
+				num++;
+			}
+			System.out.println(result);
+	}
+
+}
